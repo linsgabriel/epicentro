fdca51b66cd2c91e61437da25fef6bb0b998c501
Jessica Milena <jessicamilenacorrea@gmail.com>
2015-07-30 10:18:28 -0300

    correção do index.php

--- index.php
+++ index.php
@@ -53,8 +53,8 @@
   <?php include 'modules/jumbotron/index.html' ?>
   <?php include 'modules/menu/index.html' ?>
   <main>
-    <?php include 'modules/servico/index.html' ?>
-    <?php include 'modules/portfolio/index.html' ?>
+    <?php include 'modules/servicos/index.html' ?>
+    <?php include 'modules/portifolio/index.html' ?>
     <?php include 'modules/frases/index.html' ?>
     <?php include 'modules/equipe/index.html' ?>
     <?php include 'modules/contato/index.html' ?>
