3c1c0c2d3b78c139bfe432647d6f11f765a6869d
João Paulo Saragossa <joaosaragossa.dev@gmail.com>
2015-08-03 09:45:36 -0300

    CSS Jumbotron #51

--- modules/jumbotron/index.php
+++ modules/jumbotron/index.php
@@ -1,11 +1,11 @@
-<header class="header">
-  <h1>Epicentro Tecnologia</h1>
+<header class="jumbotron">
+  <h1 class="hidden">Epicentro Tecnologia</h1>
   <?php include 'modules/menu/index.html' ?>
   <div class="slider">
     <img src="assets/img/Jumbotron/jumbotron.jpg" alt="jumbotron">
   </div>
   <div class="frases">
-    <p>A Epicentro Tecnologia desenvolve soluções em páginas web, sistemas de gerenciamento de conteúdo e design gráfico</p>
+    <p class="active">A Epicentro Tecnologia desenvolve soluções em páginas web, sistemas de gerenciamento de conteúdo e design gráfico</p>
     <p>Sites 100% responsivos, administração intuitiva com WordPress, integração com redes sociais e otimização para o Google Analytics</p>
     <p>Produzimos seu projeto do design ao conteúdo, personalizado para as suas necessidades e focado na obtenção de resultados</p>
   </div>
