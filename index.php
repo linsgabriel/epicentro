5a58587a52585855337da93497c64833fa8b17e6
gabriel <linsgabriel87@gmail.com>
2015-08-28 12:08:39 -0300

    Slider jumbotron

--- index.php
+++ index.php
@@ -66,5 +66,6 @@
   </main>
 </body>
 <script src="assets/js/jquery-1.11.3.min.js"></script>
+<script src="assets/js/unslider.min.js"></script>
 <script src="scripts.js"></script>
 </html>
--- modules/jumbotron/script.js
+++ modules/jumbotron/script.js
@@ -1,25 +1,3 @@
-
-// Jumbotron
-
-var frase = $('.jumbotron .frases p');
-
-var i = 0;
-var length = $('.jumbotron .frases p').length;
-
-// console.log(length);
-
-// setInterval(function() {
-
-//   // console.log(frase[0]);
-
-//   console.log(i);
-
-//   if (i < length - 1) {
-
-//     i++;
-
-//   } else {
-
-//     i = 0;
-//   }
-// },1000);
+$(function() {
+    $('.banner').unslider();
+});
