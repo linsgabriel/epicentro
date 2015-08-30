09e710a041b620f787a20c9174b0c887eaa30066
aa779 <aa779@hotmail.com>
2015-08-29 21:23:58 -0300

    Comportamentos Contato #147

---
+++ modules/contato/script.js
@@ -0,0 +1,74 @@
+hidemap();
+
+function hidemap() {
+  var c = document.getElementById('ifr_mapa');
+  var d = document.getElementById('iframe');
+  var e = document.getElementById('fechamapa');
+  var f = document.getElementById('formul');
+  
+  var browser = navigator.appName;
+  
+  c.style.visibility = "hidden";
+  c.style.display = "none";
+  d.style.visibility = "hidden";
+  d.style.display = "none";
+  e.innerHTML = "<a class='close' onclick='showmap()'>Exibir mapa</a>";
+  var nav = detect();
+  if (nav == "Chrome")
+  {
+    f.style.visibility = "initial";
+    var g = document.getElementsByClassName('menu full');
+    g.style.visibility = "initial";
+  }
+  else
+    f.style.top = "0";
+}
+
+function showmap() {
+  var c = document.getElementById('ifr_mapa');
+  var d = document.getElementById('iframe');
+  var e = document.getElementById('fechamapa');
+  var f = document.getElementById('formul');
+
+  var browser = navigator.appName;
+  
+  c.style.visibility = "visible";
+  c.style.display = "initial";
+  d.style.visibility = "visible";
+  d.style.display = "initial";
+  e.innerHTML = "<a class='close' onclick='hidemap()'>Fechar mapa</a>";
+
+  var nav = detect();
+  if (nav == "Chrome")
+  {
+    scroll();
+    f.style.visibility = "hidden";
+    var g = document.getElementsByClassName('menu full');
+    g.style.visibility = "initial";
+  }
+  else
+    f.style.top = "60%";
+}
+
+function detect () {
+  var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+  var isFirefox = typeof InstallTrigger !== 'undefined';
+  var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+  var isChrome = !!window.chrome && !isOpera;
+  var isIE = /*@cc_on!@*/false || !!document.documentMode;
+  
+  if (isOpera)
+    return "Opera";
+  else if (isFirefox)
+    return "Firefox";
+  else if (isSafari)
+    return "Safari";
+  else if (isChrome)
+    return "Chrome";
+  else if (isIE)
+    return "Explorer";
+}
+
+function scroll() {
+    window.scrollBy(0, -70);
+}
